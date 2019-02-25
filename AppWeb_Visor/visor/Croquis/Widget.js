5429bee67b4edfafd85880f97a3c81c51a932330
Cristian Rodriguez <cristianrrb@gmail.com>
2019-02-25 16:48:28 -0300

    Update Widget.js

--- AppWeb_Visor/visor/Croquis/Widget.js
+++ AppWeb_Visor/visor/Croquis/Widget.js
@@ -90,7 +90,13 @@ function(
 
                 this.initDropZone();
 
-                this.simbolo = new SimpleFillSymbol(this.config.simboloArea);
+                // this.simbolo = new SimpleFillSymbol(this.config.simboloArea);
+
+                this.simbolo = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, 
+                    new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                    new Color([98,194,204]), 2), new Color([98,194,204,0.5])
+                );
+
                 this.capaGrafica = new GraphicsLayer();
                 this.map.addLayer(this.capaGrafica);
 
@@ -364,13 +370,6 @@ function(
                 this.capaManzanas.queryFeatures(query, lang.hitch(this, function(result){
                     var extent = graphicsUtils.graphicsExtent(result.features);
                     this.map.setExtent(extent, true);
-                    // this.map.infoWindow.setFeatures(result.features);
-
-                    var symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, 
-                        new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
-                        new Color([98,194,204]), 2), new Color([98,194,204,0.5])
-                    );
-
                     arrayUtils.forEach(result.features, function(feature) {
                         var graphic = feature;
                         graphic.setSymbol(symbol);
