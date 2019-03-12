17c6b072441ce0bb5caba474007cac8b8b46f832
Cristian Rodriguez <cristianrrb@gmail.com>
2019-03-12 11:56:27 -0300

    Create util.py

---
+++ util.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-1 -*-
+import arcpy
+import datetime
+
+def mensaje(m):
+    n = datetime.datetime.now()
+    s = "[{}]: {}".format(n.strftime("%H:%M:%S"), m)
+    print(s)
+    arcpy.AddMessage(s)
+
